11728970abd9fcf63661331ce85a51118e8b6225
guxiaochang01_dxm <is_gxc@163.com>
2024-05-05 22:12:16 +0800

    axios基础封装

--- src/main.js
+++ src/main.js
@@ -5,6 +5,12 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+import { getCategory } from './apis/testAPI'
+
+//测试接口函数
+getCategory().then(res => {
+    console.log(res)
+})
 
 const app = createApp(App)
 
