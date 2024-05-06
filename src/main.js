79d0bc69bbaaebafc149038a5d925674c8f2d1c7
guxiaochang01_dxm <is_gxc@163.com>
2024-05-06 23:12:07 +0800

    吸顶导航的交互实现

--- src/main.js
+++ src/main.js
@@ -6,12 +6,6 @@ import App from './App.vue'
 import router from './router'
 //引入初始化样式文件
 import '@/styles/common.scss'
-import { getCategory } from './apis/testAPI'
-
-//测试接口函数
-getCategory().then(res => {
-    console.log(res)
-})
 
 const app = createApp(App)
 
