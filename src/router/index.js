3c8ccaa97f9352716b5bfaabd23a80f611719acf
guxiaochang01_dxm <is_gxc@163.com>
2024-05-19 15:55:12 +0800

    二级分类-整体认识路由配置

--- src/router/index.js
+++ src/router/index.js
@@ -5,6 +5,7 @@ import Login from '@/views/Login/index.vue'
 import Layout from '@/views/Layout/index.vue'
 import Home from '@/views/Home/index.vue'
 import Category from '@/views/Category/index.vue'
+import SubCategory from '@/views/SubCategory/index.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -16,11 +17,15 @@ const router = createRouter({
       children:[
         {
           path:'',
-          component:Home
+          component: Home
         },
         {
           path:'category/:id',
-          component:Category
+          component: Category
+        },
+        {
+          path:'category/sub/:id',
+          component: SubCategory
         }
       ]
     },
