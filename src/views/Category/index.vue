b887f52d2637304a9a438a7a07f50026ae5c24e8
guxiaochang01_dxm <is_gxc@163.com>
2024-05-19 15:36:58 +0800

    一级分类-按照业务逻辑拆分函数

--- src/views/Category/index.vue
+++ src/views/Category/index.vue
@@ -1,38 +1,11 @@
 <script setup>
-import { getCategoryAPI } from '@/apis/category'
-import { ref, onMounted } from 'vue'
-import { useRoute } from 'vue-router'
-import { getBannerAPI } from '@/apis/home'
 import GoodsItem from '../Home/components/GoodsItem.vue'
-import { onBeforeRouteUpdate } from 'vue-router'
+import { useBanner } from './composables/useBanner'
+import { useCategory } from './composables/useCategory';
 
-// 获取数据
-const categoryData = ref({})
-const route = useRoute()
-const getCategory = async (id = route.params.id) => {
-    const res = await getCategoryAPI(id)
-    categoryData.value = res.result
-}
+const {bannerList} =  useBanner()
+const {categoryData} = useCategory()
 
-onMounted(() => getCategory())
-
-// 目标： 路由参数变化的时候  可以把分类数据接口重新发送
-onBeforeRouteUpdate((to)=>{
-  // 存在问题： 使用最新的路由参数请求最新的分类数据
-  getCategory(to.params.id)
-})
-
-// 获取banner
-const bannerList = ref([])
-
-const getBanner = async() => {
-    const res = await getBannerAPI({
-        distributionSite: '2'
-    })
-    bannerList.value = res.result
-}
-
-onMounted(()=> getBanner())
 </script>
 
 <template>
